a564a48829d42554f9009138ce39942bc7d8b752
EduardooPV <eduardoveltroni@hotmail.com>
2025-02-25 21:16:40 -0300

    feat: add security in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,31 @@
 import express from 'express';
 import authRouter from './routes/auth';
+import helmet from 'helmet';
+import cors from 'cors';
+import rateLimit from 'express-rate-limit';
+import hpp from 'hpp';
 
 const app = express();
-app.use(express.json());
+
+const jsonOptions = express.json({ limit: '10kb' });
+
+const corsOptions = cors({
+  origin: 'https://frontend.com',
+  optionsSuccessStatus: 200,
+});
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message:
+    'Muitas requisições vindas deste IP, por favor, tente novamente mais tarde.',
+});
+
+app.use(jsonOptions);
+app.use(corsOptions);
+app.use(limiter);
+app.use(helmet());
+app.use(hpp());
 
 app.use('/auth', authRouter);
 
