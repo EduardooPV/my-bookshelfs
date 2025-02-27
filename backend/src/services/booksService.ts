2dc113c2e50dff98eb676329b56ba173ba4317a4
LuizVeltroni <luiz.veltroni@warren.com.br>
2025-02-27 08:59:37 -0300

    feat: list props

--- backend/src/services/booksService.ts
+++ backend/src/services/booksService.ts
@@ -1,6 +1,48 @@
-export const getAllBooks = async () => {
+export const getBook = async (id: string) => {
+  const baseUrl = 'https://openlibrary.org/';
+  const query = 'the lord of the rings';
+  const fields = [
+    'key',
+    'title',
+    'author_name',
+    'cover_edition_key',
+    'number_of_pages_median',
+    'ratings_average',
+    'first_publish_year',
+  ].join(',');
+
   const response = await fetch(
-    'https://openlibrary.org/search.json?q=the+lord+of+the+rings&&fields=key,title,author_name',
+    `${baseUrl}/${id}?q=${encodeURIComponent(query)}&fields=${fields}`,
+  );
+
+  const books = await response.json();
+
+  if (!response.ok) {
+    throw new Error('Erro ao buscar livros');
+  }
+
+  return books;
+};
+
+export const getAllBooks = async () => {
+  const baseUrl = 'https://openlibrary.org/search.json';
+  const query = 'the lord of the rings';
+  const fields = [
+    'key',
+    'title',
+    'author_name',
+    'cover_edition_key',
+    'first_publish_year',
+    'ratings_average',
+    'readinglog_count',
+    'want_to_read_count',
+    'currently_reading_count',
+    'already_read_count',
+    'first_publish_year',
+  ].join(',');
+
+  const response = await fetch(
+    `${baseUrl}?q=${encodeURIComponent(query)}&fields=${fields}`,
   );
 
   const books = await response.json();
